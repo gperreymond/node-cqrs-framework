b6a3dd7e704bd486dda8936b57d50ccff17bc99e
Gilles Perreymond <gperreymond@viadeoteam.com>
2016-10-14 17:18:01 +0200

    circle+docker

--- test/integration/000-initialization/rethinkdb-structure.test.js
+++ test/integration/000-initialization/rethinkdb-structure.test.js
@@ -6,12 +6,9 @@ const path = require('path')
 const basedir = path.resolve(__dirname, '../../..')
 
 const structure = require(path.resolve(basedir, 'example/lib/rethinkdb')).structure
-const config = require(path.resolve(basedir, 'example/lib/config'))
 
 describe('[integration] rethinkdb structure', function () {
   it('should be a success', function (done) {
-    console.log(config.get('CQRS_RABBITMQ_URL'))
-    console.log(config.get('CQRS_RETHINKDB_HOST'))
     structure()
       .then(() => {
         done()
