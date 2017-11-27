4f66bb6dbdaa46fa75f9688a680a124a77281f9c
Gilles Perreymond <gperreymond@gmail.com>
2017-11-27 17:53:40 +0100

    refactoring client/server

--- examples/client.js
+++ examples/client.js
@@ -3,12 +3,27 @@ const Client = require('..').Client
 const client = new Client()
 
 client
+  .listen()
+  .ready(error => {
+    if (error) {
+      console.log('cient error')
+      console.log(error)
+      process.exit(1)
+    }
+    console.log('cient ready')
+    client.close()
+  })
+
+/* client
+  .subscribe('*.Success', (result) => {
+    console.log(result)
+  })
   .initialize()
   .then(() => {
     console.log('cient initialize success')
-    client.__bus.publish('BasicNopeCommand', {command: true})
-    client.__bus.publish('BasicNopeQuery', {query: true})
+    client.publish('BasicNopeCommand', {command: true})
+    client.publish('BasicNopeQuery', {query: true})
   }).catch(error => {
     console.log(error)
     process.exit(1)
-  })
+  }) */
